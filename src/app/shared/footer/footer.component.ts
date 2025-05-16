89ea0233c9b13d1ebee6c826513db860cbba31fc
jaztorres <jazmin.torres@myma.mx>
2025-05-16 11:13:48 -0600

    update

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { AvisoComponent } from '../../legales/aviso/aviso.component';
 import { BasesComponent } from '../../legales/bases/bases.component';
-import { TerminosComponent } from '../../legales/terminos/terminos.component';
+import { AvisoComponent } from '../../legales/aviso/aviso.component';
+
+
 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, AvisoComponent,BasesComponent],
+  imports: [RouterLink,BasesComponent,AvisoComponent],
   templateUrl: './footer.component.html',
   styles: ``
 })
