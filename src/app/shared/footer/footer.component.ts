78ef4209c16f03da476b4fd839ca14be66bf15b6
jaztorres <jazmin.torres@myma.mx>
2025-05-19 09:52:38 -0600

    footer update

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+
 import { BasesComponent } from '../../legales/bases/bases.component';
 import { AvisoComponent } from '../../legales/aviso/aviso.component';
 
