eef2ac9c65af72eba4c0c7e3c3f014bb6b3953e5
jaztorres <jazmin.torres@myma.mx>
2025-05-27 17:11:31 -0600

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,5 @@ import { FooterComponent } from './shared/footer/footer.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'webRepsol';
+  title = 'webAlk';
 }
